13244152423fe5b098e0079c2cd4316d1619e85a
li <1021678253@qq.com>
2018-11-15 18:08:14 +0800

    v1.0：新建测试类

---
+++ demo/src/main/java/com/example/demo/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Administrator on 2018-11-15.
+ */
+
+@Controller
+public class HelloWorld {
+    public String test(){
+        return "test";
+    }
+}
